9ccc2ac7b3acfe02404854a8547f4b6f2a3c8808
EXL <exlmotodev@gmail.com>
2020-06-08 21:19:55 +0700

    Fix game logic in C

--- src/2048.c
+++ src/2048.c
@@ -1,26 +1,30 @@
 #include "2048.h"
 
-#include <stdlib.h>
 #include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define forl int i = 0; for (; i < HORIZONTAL; ++i)
 #define forb int i = 0; for (; i < BOARD_SIZE; ++i)
 
 int board[BOARD_SIZE];
-int b_reg[HORIZONTAL];
-int f_reg[HORIZONTAL];
 int *space[BOARD_SIZE];
+int b_reg[HORIZONTAL], f_reg[HORIZONTAL];
 
-int win, lose;
-int score;
+int win, lose, score;
+int K_ESCAPE, K_LEFT, K_RIGHT, K_UP, K_DOWN;
 
-inline double math_random() { return rand() / (double) RAND_MAX; }
-inline double degrees_to_radians(int degrees) { return ((degrees) * M_PI / 180.0); }
-inline int tile_at(int x, int y) { return board[x + y * 4]; }
+static inline double math_random() { return rand() / (double) RAND_MAX; }
+static inline double degrees_to_radians(int degrees) { return ((degrees) * M_PI / 180.0); }
+static inline int tile_at(int x, int y) { return board[x + y * 4]; }
 
-int *board_p() { return board; }
-unsigned foreground(int value) { return (value < 16) ? 0x00776E65 : 0x00F9F6F2; }
-unsigned background(int value) {
+int *e_board() { return board; }
+int e_win() { return win; }
+int e_lose() { return lose; }
+int e_score() { return score; }
+unsigned e_foreground(int value) { return (value < 16) ? 0x00776E65 : 0x00F9F6F2; }
+
+unsigned e_background(int value) {
 	switch (value) {
 		case    2: return 0x00EEE4DA;
 		case    4: return 0x00EDE0C8;
@@ -35,6 +39,15 @@ unsigned background(int value) {
 		case 2048: return 0x00EDC22E;
 	}
 	return 0x00CDC1B4;
+}
+
+void e_init(int escape, int left, int right, int up, int down) {
+	K_ESCAPE = escape;
+	K_LEFT = left;
+	K_RIGHT = right;
+	K_UP = up;
+	K_DOWN = down;
+	srand(time(NULL));
 }
 
 void set_line(int index, int *line) {
@@ -63,7 +76,7 @@ void copy_line(int *dest, int *src) {
 int *move_line(int *line) {
 	int size = 0;
 	forl
-		if (!line[i])
+		if (line[i])
 			f_reg[size++] = line[i];
 	if (!size)
 		copy_line(f_reg, line);
@@ -119,7 +132,7 @@ void reset_regs() {
 		b_reg[i] = f_reg[i] = 0;
 }
 
-void reset() {
+void e_reset() {
 	score = win = lose = 0;
 	forb
 		board[i] = 0;
@@ -131,18 +144,20 @@ void reset() {
 
 void rotate(int angle) {
 	int new_board[BOARD_SIZE];
-	int offset_x = 3, offset_y = 3, x = 0, y = 0;
+	int offset_x = 3, offset_y = 3, x = 0;
 	if (angle == 90)
 		offset_y = 0;
 	else if (angle == 270)
 		offset_x = 0;
 	const double rad = degrees_to_radians(angle);
-	const int cs = cos(rad), sn = sin(rad);
-	for (; x < HORIZONTAL; ++x)
+	const int cs = (int) cos(rad), sn = (int) sin(rad);
+	for (; x < HORIZONTAL; ++x) {
+		int y = 0;
 		for (; y < VERTICAL; ++y) {
 			int new_x = (x * cs) - (y * sn) + offset_x, new_y = (x * sn) + (y * cs) + offset_y;
-			new_board[new_x + new_y * HORIZONTAL] = tile_at(x, y);
+			new_board[new_x + new_y * 4] = tile_at(x, y);
 		}
+	}
 	forb
 		board[i] = new_board[i];
 }
@@ -178,4 +193,32 @@ void down() {
 	rotate(90);
 	left();
 	rotate(270);
+}
+
+int can_move() {
+	if (update_space())
+		return 1;
+	int x = 0;
+	for (; x < HORIZONTAL; ++x) {
+		int y = 0;
+		for (; y < VERTICAL; ++y)
+			if ((x < 3 && tile_at(x, y) == tile_at(x + 1, y)) || (y < 3 && tile_at(x, y) == tile_at(x, y + 1)))
+				return 1;
+	}
+	return 0;
+}
+
+void e_key_event(int key) {
+	if (key == K_ESCAPE)
+		e_reset();
+	if (!can_move())
+		lose = 1;
+	if (!win && !lose) {
+		if (key == K_LEFT) left();
+		else if (key == K_RIGHT) right();
+		else if (key == K_UP) up();
+		else if (key == K_DOWN) down();
+	}
+	if (!win && !can_move())
+		lose = 1;
 }
--- src/2048.h
+++ src/2048.h
@@ -1,18 +1,35 @@
 #ifndef H_2048
 #define H_2048
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define HORIZONTAL         4
 #define VERTICAL           4
 #define BOARD_SIZE         HORIZONTAL * VERTICAL
 #define END_GAME_TARGET    2048
 
-extern void reset();
-extern void left();
-extern void right();
-extern void up();
-extern void down();
-extern unsigned foreground(int value);
-extern unsigned background(int value);
-extern int *board_p();
+extern void e_init(int escape, int left, int right, int up, int down);
+extern void e_reset();
+//extern void left();
+//extern void right();
+//extern void up();
+//extern void down();
+
+extern unsigned e_foreground(int value);
+extern unsigned e_background(int value);
+
+extern int *e_board();
+extern int e_win();
+extern int e_lose();
+extern int e_score();
+extern void e_key_event(int key);
+//extern void set_win(int res);
+//extern void set_lose(int res);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // H_2048
