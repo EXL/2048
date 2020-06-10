ca6afe645ad6301bf6b0696305eca7dd614d9988
EXL <exlmotodev@gmail.com>
2020-06-10 20:14:42 +0700

    Add MotoMAGX build files

---
+++ 2048-MotoMAGX/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+func_build() {
+	make clean
+	make PLATFORM=EZX-Z6; make rmobj
+	make PLATFORM=EZX-V8; make rmobj
+	make PLATFORM=EZX-Z6W; make rmobj
+	make PLATFORM=EZX-U9; make rmobj
+	make PLATFORM=EZX-ZN5; make rmobj
+	make PLATFORM=EZX-EM30; make rmobj
+	make PLATFORM=EZX-E8; make rmobj
+	make PLATFORM=EZX-VE66; make rmobj
+	make PLATFORM=EZX-EM35; make rmobj
+}
+
+func_build;
+exit 1
