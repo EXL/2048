1954cd91a5af0a9666fa3313b049ee5572b9fc17
EXL <exlmotodev@gmail.com>
2020-06-10 21:56:47 +0700

    Fix tiles for MotoMAGX platform

--- 2048-MotoMAGX/2048-MotoMAGX.cpp
+++ 2048-MotoMAGX/2048-MotoMAGX.cpp
@@ -53,16 +53,16 @@ class Widget : public QWidget {
 		painter.setBrush(QColor(e_background(value)));
 #if defined(EZX_Z6W) || defined(EZX_ZN5) || defined(EZX_U9)
 		// HACK: Emulating drawRoundRect() method in 9 calls.
-		const int w = TILE_SIZE / 2, dw = w * 2, rad = w / 4, rw = rad * 6, rect = w - rad;
+		const int w = TILE_SIZE / 2, dw = w * 2, qw = w / 4, rad = qw * 2, rw = rad * 3, rect = w - rad;
 		painter.drawPie(xOffset, yOffset, rect, rect, -16*180, -16*90);
-		painter.drawPie(xOffset + w + rad, yOffset, rect, rect, 16*90, -16*90);
+		painter.drawPie(xOffset + w + rad - 1, yOffset, rect, rect, 16*90, -16*90);
 		painter.drawPie(xOffset, yOffset + w + rad, rect, rect, -16*90, -16*90);
-		painter.drawPie(xOffset + w + rad, yOffset + w + rad, rect, rect, 0, -16*90);
-		painter.drawRect(xOffset + rad, yOffset + rad, TILE_SIZE - rad * 2, TILE_SIZE - rad * 2);
-		painter.drawRect(xOffset, yOffset + rad, rad, rw);
-		painter.drawRect(xOffset + dw - rad, yOffset + rad, rad, rw);
-		painter.drawRect(xOffset + rad, yOffset, rw, rad);
-		painter.drawRect(xOffset + rad, yOffset + dw - rad, rw, rad);
+		painter.drawPie(xOffset + w + rad - 1, yOffset + w + rad, rect, rect, 0, -16*90);
+		painter.drawRect(xOffset + qw, yOffset + qw, TILE_SIZE - rad, TILE_SIZE - rad);
+		painter.drawRect(xOffset, yOffset + qw, qw, rw);
+		painter.drawRect(xOffset + dw - qw - 2, yOffset + qw, qw, rw);
+		painter.drawRect(xOffset + qw - 1, yOffset, rw + 1, qw);
+		painter.drawRect(xOffset + qw, yOffset + dw - qw - 1, rw + 1, qw);
 #else
 		painter.drawRoundRect(xOffset, yOffset, TILE_SIZE, TILE_SIZE, 20, 20);
 #endif
