397c8b071d42d1b26c0f89207cb9af4a70de35a7
EXL <exlmotodev@gmail.com>
2020-06-29 11:03:29 +0700

    Add comments and strip for Serenity port

--- 2048-Serenity/port/2048/package.sh
+++ 2048-Serenity/port/2048/package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash ../.port_include.sh
 port=2048
 version=git
+# TODO: Replace this link with github.com/EXL/2048 on public release.
 files="http://esxi.z-lab.me:666/~exl_lab/software/2048.zip 2048-${version}.zip"
 workdir=2048
 makeopts="-C 2048-Serenity/port -f Makefile.port"
