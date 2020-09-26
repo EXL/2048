c495c25e3a2d06f5e611318922910e82c97114f0
EXL <exlmotodev@gmail.com>
2020-09-26 09:46:25 +0700

    Fix link again

--- 2048-Serenity/port/2048-Serenity/package.sh
+++ 2048-Serenity/port/2048-Serenity/package.sh
@@ -2,7 +2,7 @@
 port=2048-Serenity
 version=git
 # TODO: Replace this link with github.com/EXL/2048 on public release.
-files="https://lab.exlmoto.ru/2048.zip 2048-${version}.zip"
+files="https://exlmoto.ru/2048.zip 2048-${version}.zip"
 workdir=2048-Serenity
 makeopts="-C 2048-Serenity/port -f Makefile.port"
 installopts="-C 2048-Serenity/port -f Makefile.port"
