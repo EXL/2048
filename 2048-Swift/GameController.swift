33c99bb4c7aec5403c512f58c9e19dc71b173ed9
EXL <exlmotodev@gmail.com>
2020-11-10 08:21:13 +0700

    Initial 2048-Swift project

---
+++ 2048-Swift/GameController.swift
@@ -0,0 +1,14 @@
+//
+//  GameController.swift
+//  2048-Swift
+//
+//  Created by EXL on 11/10/20.
+//  Copyright © 2020 EXL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class GameController: NSObject, NSApplicationDelegate {
+	
+}
