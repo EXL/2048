2888082c39c4f0050586e6620960e3334b5aa760
EXL <exlmotodev@gmail.com>
2020-11-13 14:24:29 +0700

    Implement Save State static function

--- 2048-Swift/GameController.swift
+++ 2048-Swift/GameController.swift
@@ -12,8 +12,29 @@ import Cocoa
 class GameController: NSObject, NSApplicationDelegate {
 	@IBOutlet weak var gameView: GameView!
 
-	// TODO: Save static func.
+	static func saveState() -> String {
+		let formatter = DateFormatter()
+		formatter.dateStyle = .long
+		formatter.timeStyle = .long
+		let strState = String(format: "GMStateFMT".localized, formatter.string(from: Date()))
+		UserDefaults.standard.set(strState, forKey: "saveDate")
+		for i in 0..<BOARD_SIZE {
+			UserDefaults.standard.set(p_board![Int(i)], forKey: String(format: "saveBoard_%d", i))
+		}
+		UserDefaults.standard.set(e_score, forKey: "saveScore")
+		UserDefaults.standard.set((e_win as NSNumber).boolValue, forKey: "saveWin")
+		UserDefaults.standard.set((e_lose as NSNumber).boolValue, forKey: "saveLose")
+		return (UserDefaults.standard.synchronize()) ? strState : ""
+	}
+
 	// TODO: Load static func.
+	static func loadState() -> String {
+		let formatter = DateFormatter()
+		formatter.dateStyle = .long
+		formatter.timeStyle = .long
+		let strState = String(format: "GMStateFMT".localized, formatter.string(from: Date()))
+		return strState
+	}
 
 	func applicationDidFinishLaunching(_ notification: Notification) {
 		gameView.window?.makeKeyAndOrderFront(self)
