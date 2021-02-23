9affe8a1fdefdbe74b43904690a8f8579c59186d
EXL <exlmotodev@gmail.com>
2021-02-24 03:00:26 +0700

    Passthrough board and keys

--- 2048-SMD/src/main.c
+++ 2048-SMD/src/main.c
@@ -1,15 +1,55 @@
+#include "2048.h"
+
 #include <genesis.h>
 
-int main(bool hardReset)
-{
-    VDP_drawText("Hello world !", 12, 12);
+static void displayBoard() {
+    int y = 0, x;
+    char tile[5];
 
-    while(TRUE)
-    {
-        // nothing to do here
-        // ...
+    for (; y < LINE_SIZE; ++y)
+        for (x = 0; x < LINE_SIZE; ++x) {
+            intToStr(e_board[x + y * LINE_SIZE], tile, 5);
+            VDP_drawText(tile, x * 6, y * 4);
+        }
 
-        // always call this method at the end of the frame
+    /*
+    intToStr(j_joy, tile, 5);
+    VDP_drawText(tile, 0, y * 5);
+    intToStr(j_change, tile, 5);
+    VDP_drawText(tile, 6, y * 5);
+    intToStr(j_state, tile, 5);
+    VDP_drawText(tile, 12, y * 5);
+    intToStr(j_pressed, tile, 5);
+    VDP_drawText(tile, 18, y * 5);
+    */
+}
+
+static void joyEvent(u16 joy, u16 changed, u16 state) {
+    const u16 value = changed & state;
+
+    if (value & BUTTON_LEFT)
+        e_key(BUTTON_LEFT);
+    else if (value & BUTTON_RIGHT)
+        e_key(BUTTON_RIGHT);
+    else if (value & BUTTON_UP)
+        e_key(BUTTON_UP);
+    else if (value & BUTTON_DOWN)
+        e_key(BUTTON_DOWN);
+    else if (value & BUTTON_START || value & BUTTON_A || value & BUTTON_B)
+        e_key(BUTTON_START);
+}
+
+int main(bool hardReset) {
+    e_init(BUTTON_START, BUTTON_LEFT, BUTTON_RIGHT, BUTTON_UP, BUTTON_DOWN);
+//    updateBoard();
+
+    JOY_setEventHandler(joyEvent);
+
+    while(TRUE) {
+//        handleInput();
+
+        displayBoard();
+
         SYS_doVBlankProcess();
     }
 
