2eb5c30cbfd8e56827147f22f21fab8fe67f11af
EXL <exlmotodev@gmail.com>
2021-02-26 02:55:40 +0700

    Add MotoMAGX build script

---
+++ util/build_mgx_package.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+date=`date "+%d-%b-%Y"`
+
+function clean {
+	rm -Rf util/2048/
+}
+
+function build_mgx {
+	mkdir -p util/2048
+	cp 2048-MotoMAGX/2048-MotoMAGX_ZN5 util/2048
+	cp 2048-MotoMAGX/mgx/2048.cfg util/2048
+	cp image/icon/ic_launcher0.png util/2048/icon_usr.png
+	cd util/
+	7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on 2048.7z 2048/
+	mv 2048.7z "../2048_MotoMAGX_ZN5_v1.0_$date.mgx"
+	cd -
+}
+
+function main {
+	cd "`dirname $0`/../"
+	build_mgx
+	clean
+	cd -
+	exit
+}
+
+main
