331bef37f24df8424ff6c44072cb41c7528c4252
EXL <exlmotodev@gmail.com>
2021-05-04 05:49:27 -0400

    [2048-EZX]: Port 2048 game from 2048-MotoMAGX

--- 2048-EZX/2048-EZX.cpp
+++ 2048-EZX/2048-EZX.cpp
@@ -2,85 +2,50 @@
 
 #include <ZApplication.h>
 #include <ZMainWidget.h>
-//#include <ZKbMainWidget.h>
-//#include <ZHeader.h>
-//#include <ZSoftKey.h>
-//#include <ZKeyDef.h>
-//#include <ZMessageDlg.h>
-//#include <ZOptionsMenu.h>
-#include <ZGlobal.h>
-//#include <ZNoticeDlg.h>
+#include <ZMessageBox.h>
 
-#include <ezxres.h> // TODO: ???
+#include <ezxres.h>
 #include <ezxutilcst.h>
 
 #include <qfileinfo.h>
+#include <qlabel.h>
 #include <qtextcodec.h>
 #include <qtimer.h>
-#include <qlabel.h>
 
 #include <ctime>
 
-
-
-//#if defined(EZX_Z6) || defined (EZX_V8)
-//	#define KEYCODE_0                              EZX_KEY_0
-//	#define KEYCODE_2                              EZX_KEY_2
-//	#define KEYCODE_4                              EZX_KEY_4
-//	#define KEYCODE_6                              EZX_KEY_6
-//	#define KEYCODE_8                              EZX_KEY_8
-	#define KEYCODE_UP                             Qt::Key_Up
-	#define KEYCODE_DOWN                           Qt::Key_Down
-	#define KEYCODE_LEFT                           Qt::Key_Left
-	#define KEYCODE_RIGHT                          Qt::Key_Right
-	#define KEYCODE_CLEAR                          1000
-// TODO: Check keys!!!
-//	#ifdef EZX_V8
-//	#define MAINDISPLAY_HEADER                     TINY_TYPE
-//	#define TypeOK                                 just_ok
-//	#endif
-//#endif
-
-#if defined(EZX_EM30) || defined (EZX_E8)
-const int TILE_SIZE = 40;
-const int FIELD_OFFSET_SCALE = 5;
-#else
-const int TILE_SIZE = 48;
 const int FIELD_OFFSET_SCALE = 32;
-#endif
-const int TILE_MARGIN = 5;
+const int TILE_SIZE          = 48;
+const int TILE_MARGIN        = 5;
+
+enum { M_SCREEN, M_SAVE, M_LOAD, M_RESET, M_SOUND, M_ROUNDED, M_RECTANGLE, M_TILES, M_ABOUT, M_EXIT };
 
 class Widget : public QWidget {
 	Q_OBJECT
 
 	QPixmap *fb;
-	QFont *font_large, *font_middle, *font_normal, *font_small;
+	QFont *font_large, *font_middle, *font_small;
 	int ww, hh;
+	int score;
+	bool rounded;
 
 	inline int offsetCoords(int coord) { return coord * (TILE_MARGIN + TILE_SIZE) + TILE_MARGIN * 2; }
 	void drawTile(QPainter &painter, int value, int x, int y) {
-		const int xOffset = offsetCoords(x) + ww / FIELD_OFFSET_SCALE, yOffset = offsetCoords(y);
+		const int xOffset = offsetCoords(x) + ww / FIELD_OFFSET_SCALE, yOffset = offsetCoords(y) - TILE_MARGIN - 1;
 		painter.setPen(QPen::NoPen);
 		painter.setBrush(QColor(e_background(value)));
-#if defined(EZX_Z6W) || defined(EZX_ZN5) || defined(EZX_U9)
-		// HACK: Emulating drawRoundRect() method in 6 calls.
-		const int w = TILE_SIZE / 2, qw = w / 4, rad = qw * 2, rw = rad * 3, rect = w - rad;
-		painter.drawPie(xOffset, yOffset, rect, rect, -16*180, -16*90);
-		painter.drawPie(xOffset + w + rad - 1, yOffset, rect, rect, 16*90, -16*90);
-		painter.drawPie(xOffset, yOffset + w + rad, rect, rect, -16*90, -16*90);
-		painter.drawPie(xOffset + w + rad - 1, yOffset + w + rad, rect, rect, 0, -16*90);
-		painter.drawRect(xOffset, yOffset + qw, TILE_SIZE - 2, rw);
-		painter.drawRect(xOffset + qw - 1, yOffset, rw + 1, TILE_SIZE - 1);
-#else
-		painter.drawRoundRect(xOffset, yOffset, TILE_SIZE, TILE_SIZE, 20, 20);
-#endif
+		if (rounded)
+			painter.drawRoundRect(xOffset, yOffset, TILE_SIZE, TILE_SIZE, 36, 36);
+		else
+			painter.drawRect(xOffset, yOffset, TILE_SIZE, TILE_SIZE);
 		if (value) {
 			const int size = (value < 100) ? 18 : (value < 1000) ? 14 : 10;
 			const QFont &font = *((value < 100) ? font_large : (value < 1000) ? font_middle : font_small);
 			const QString strValue = QString("%1").arg(value);
 			painter.setPen(QColor(e_foreground(value)));
 			painter.setFont(font);
-			const int w = QFontMetrics(font).width(strValue), h = (value < 100) ? size - 4 : size - 3;
+			const int wo = (value < 100) ? 1 : (value < 1000) ? 0 : (-1), ho = (value > 1000) ? 0 : 3;
+			const int w = QFontMetrics(font).width(strValue) - wo, h = size - ho;
 			painter.drawText(xOffset + (TILE_SIZE - w) / 2, yOffset + TILE_SIZE - (TILE_SIZE - h) / 2 - 2, strValue);
 		}
 	}
@@ -93,52 +58,57 @@ class Widget : public QWidget {
 			const int w = QFontMetrics(*font_large).width(center);
 			painter.drawText(ww / 2 - w / 2, hh / 2, center);
 		}
-		painter.setPen(QColor(COLOR_TEXT));
-		painter.setFont(*font_normal);
-		const QString strScore = QString("Score: %1").arg(e_score);
-		const int w = QFontMetrics(*font_normal).width(strScore);
-		painter.drawText(TILE_MARGIN, hh - 10, "Press '0' to Reset!");
-		painter.drawText(ww - w - TILE_MARGIN, hh - 10, strScore);
+		if (score != e_score) {
+			score = e_score;
+			emit scoreChanged(score);
+		}
 	}
+signals:
+	void scoreChanged(int);
 public:
 	Widget(QWidget *parent = 0, const char *name = 0) : QWidget(parent, name, WRepaintNoErase | WResizeNoErase) {
-		e_init(KEYCODE_CLEAR, KEYCODE_LEFT, KEYCODE_RIGHT, KEYCODE_UP, KEYCODE_DOWN);
-		fb = NULL;
-		font_large = new QFont("Sans", 18, QFont::Bold);
-		font_middle = new QFont("Sans", 14, QFont::Bold);
-		font_normal = new QFont("Sans", 14, QFont::Normal);
-		font_small = new QFont("Sans", 10, QFont::Bold);
+		e_init(Qt::Key_PageUp, Qt::Key_Left, Qt::Key_Right, Qt::Key_Up, Qt::Key_Down);
+		fb = NULL; score = 0; rounded = true;
+		font_large = new QFont("Sans", 20, QFont::Bold);
+		font_middle = new QFont("Sans", 18, QFont::Bold);
+		font_small = new QFont("Sans", 16, QFont::Bold);
 		QPixmap::setDefaultOptimization(QPixmap::NormalOptim);
 		setBackgroundMode(NoBackground);
 		setFocusPolicy(QWidget::StrongFocus);
 	}
-	~Widget() { delete fb; delete font_large; delete font_middle; delete font_normal; delete font_small; }
+	~Widget() { delete fb; delete font_large; delete font_middle; delete font_small; }
+	void setRounded(bool aRounded) {
+		rounded = aRounded;
+		update();
+	}
 public slots:
 	void screenShotTimer() { QTimer::singleShot(500, this, SLOT(screenShot())); }
 	void reset() {
-		e_key(KEYCODE_CLEAR);
+		e_key(Qt::Key_PageUp);
 		update();
 	}
 	void screenShot() {
-		/*
-		QPixmap pixmap(ww, hh);
-		bitBlt(&pixmap, 0, 0, this, 0, 0, ww, hh, Qt::CopyROP, true);
+		const QWidget *parent = static_cast<QWidget *>(this->parent());
+		const int wp = parent->width(), hp = parent->height();
+		QPixmap pixmap(wp, hp);
+		bitBlt(&pixmap, 0, 0, parent, 0, 0, wp, hp, Qt::CopyROP, true);
 		const QString path = QString("%1/%2.png").arg(QFileInfo(qApp->argv()[0]).dirPath(true)).arg(time(NULL));
-		ZMessageDlg *msgDlg = new ZMessageDlg("", NULL, ZMessageDlg::TypeOK, 10*60*100);
+		const QString pathReplaced = QString(path).replace(QString("/"), " / ");
+		ZMessageBox *msgDlg = new ZMessageBox(this, NULL, QString::null, "OK");
 		if (pixmap.save(path, "PNG")) {
-			msgDlg->setTitle("Saved!");
-			msgDlg->setInstructText(QString("Screenshot Saved! Path:\n%1").arg(path));
+			msgDlg->setText(QString("<h3>Saved!</h3><br>"
+				"Screenshot Saved!<br>Path:<br><font size=\"2\">%1</font>").arg(pathReplaced));
+			msgDlg->setIconPixmap(RES_ICON_Reader().getIcon("Dialog_Complete", true));
 		} else {
-			msgDlg->setTitle("Error!");
-			msgDlg->setInstructText(QString("Error: Cannot Save Screenshot! Path: %1").arg(path));
-			msgDlg->setTitleIcon(RES_ICON_Reader().getIcon("error_pop", true));
+			msgDlg->setText(QString("<h3>Error!</h3><br>"
+				"Error: Cannot Save Screenshot!<br>Path:<br><font size=\"2\">%1</font>").arg(pathReplaced));
+			msgDlg->setIconPixmap(RES_ICON_Reader().getIcon("Dialog_Error", true));
 		}
 		msgDlg->exec();
 		delete msgDlg;
-		*/
 	}
 	void save() {
-		/*
+		ZMessageBox *msgDlg = new ZMessageBox(this, NULL, QString::null, "OK");
 		QFile save(QString("%1/save.dat").arg(QFileInfo(qApp->argv()[0]).dirPath(true)));
 		if (save.open(IO_WriteOnly)) {
 			QDateTime saveDateTime = QDateTime::currentDateTime();
@@ -147,14 +117,17 @@ public slots:
 			for (int i = 0; i < BOARD_SIZE; ++i)
 				dataStream << (Q_INT32) e_board[i];
 			dataStream << e_score; dataStream << e_win; dataStream << e_lose;
-			ZNoticeDlg::information(QString("State on:\n%1").arg(saveDateTime.toString()), "Game Saved!",
-				QString::null, "ok_pop");
-		} else
-			ZNoticeDlg::information("Cannot create save.dat file.", "Save Error!", QString::null, "error_pop");
-		*/
+			msgDlg->setText(QString("<h3>Game Saved!</h3><br>State on:\n%1").arg(saveDateTime.toString()));
+			msgDlg->setIconPixmap(RES_ICON_Reader().getIcon("Dialog_Complete", true));
+		} else {
+			msgDlg->setText(QString("<h3>Save Error!</h3><br>Cannot create save.dat file."));
+			msgDlg->setIconPixmap(RES_ICON_Reader().getIcon("Dialog_Error", true));
+		}
+		msgDlg->exec();
+		delete msgDlg;
 	}
 	void load() {
-		/*
+		ZMessageBox *msgDlg = new ZMessageBox(this, NULL, QString::null, "OK");
 		QFile save(QString("%1/save.dat").arg(QFileInfo(qApp->argv()[0]).dirPath(true)));
 		if (save.open(IO_ReadOnly)) {
 			QDateTime loadDateTime;
@@ -167,31 +140,42 @@ public slots:
 			}
 			dataStream >> score; dataStream >> win; dataStream >> lose;
 			e_score = score; e_win = win; e_lose = lose;
-			ZNoticeDlg::information(QString("State on:\n%1").arg(loadDateTime.toString()), "Game Loaded!",
-				QString::null, "ok_pop");
+			msgDlg->setText(QString("<h3>Game Loaded!</h3><br>State on:\n%1").arg(loadDateTime.toString()));
+			msgDlg->setIconPixmap(RES_ICON_Reader().getIcon("Dialog_Complete", true));
 			update();
-		} else
-			ZNoticeDlg::information("Cannot find save.dat file.", "Load Error!", QString::null, "error_pop");
-		*/
+		} else {
+			msgDlg->setText(QString("<h3>Load Error!</h3><br>Cannot find save.dat file."));
+			msgDlg->setIconPixmap(RES_ICON_Reader().getIcon("Dialog_Error", true));
+		}
+		msgDlg->exec();
+		delete msgDlg;
 	}
 protected:
 	virtual void keyPressEvent(QKeyEvent *keyEvent) {
 		QWidget::keyPressEvent(keyEvent);
 		int key = keyEvent->key();
-		/*if (key == KEYCODE_0)
-			e_key(KEYCODE_CLEAR);
-		else if (key == KEYCODE_4)
-			e_key(KEYCODE_LEFT);
-		else if (key == KEYCODE_6)
-			e_key(KEYCODE_RIGHT);
-		else if (key == KEYCODE_2)
-			e_key(KEYCODE_UP);
-		else if (key == KEYCODE_8)
-			e_key(KEYCODE_DOWN);
-		*/
-//		TODO: clear
+		if (key == Qt::Key_PageDown) // Vol "-" button.
+			e_key(Qt::Key_PageUp);   // Vol "+" button.
+		else
 			e_key(key);
-
+		repaint();
+	}
+	virtual void mousePressEvent(QMouseEvent *mouseEvent) {
+		QWidget::mousePressEvent(mouseEvent);
+		const int w4 = ww / 4, h4 = hh / 4;
+		const QRect up(w4, 0, w4 * 2, h4);
+		const QRect down(w4, h4 * 3, w4 * 2, h4);
+		const QRect left(0, h4, w4, h4 * 2);
+		const QRect right(w4 * 3, h4, w4, h4 * 2);
+		const QPoint click(mouseEvent->pos().x(), mouseEvent->pos().y());
+		if (up.contains(click))
+			e_key(Qt::Key_Up);
+		else if (down.contains(click))
+			e_key(Qt::Key_Down);
+		else if (left.contains(click))
+			e_key(Qt::Key_Left);
+		else if (right.contains(click))
+			e_key(Qt::Key_Right);
 		repaint();
 	}
 	virtual void paintEvent(QPaintEvent *) {
@@ -213,74 +197,98 @@ protected:
 class MainWidget : public ZMainWidget {
 	Q_OBJECT
 
-	void setIconPath(QString &src, const QString &dest) {
-		if (src == QString::null)
-			src = (QFile::exists(dest)) ? dest : QString::null;
-	}
+	QLabel *titleBar;
+	Widget *widget;
+	QPopupMenu *menu, *tileMenu;
+	bool sound;
+
 public slots:
 	void about() {
-		/*
-		ZMessageDlg *msgDlg = new ZMessageDlg("About 2048", QTextCodec::codecForName("UTF-8")->toUnicode(
-			"2048 Game implementation especially for MotoMAGX platform.\n\nVersion: 1.0, %1\nThanks to: Boxa, fill.sa, "
-			"VINRARUS\n© EXL (exl@bk.ru), 2020\nSource code: https://github.com/EXL/2048").arg(__DATE__),
-			ZMessageDlg::TypeOK, 10*60*100);
-		QString iconPath = QString::null;
-		setIconPath(iconPath, "/mmc/mmca1/mgxbox/2048/icon_usr.png");
-		setIconPath(iconPath, "/mmc/mmca1/mpkgbox/2048/icon_usr.png");
-		setIconPath(iconPath, "/ezxlocal/download/mystuff/mgxbox/icon_usr.png");
-		setIconPath(iconPath, "/ezxlocal/download/mystuff/mpkgbox/icon_usr.png");
-		setIconPath(iconPath, QString("%1/icon_usr.png").arg(QFileInfo(qApp->argv()[0]).dirPath(true)));
-		setIconPath(iconPath, "./icon_usr.png");
-		setIconPath(iconPath, "icon_usr.png");
-		if (iconPath != QString::null) {
-			QPixmap icon(48, 48);
+		ZMessageBox *msgDlg = new ZMessageBox(this, NULL, QString::null, "Close");
+		msgDlg->setText(QTextCodec::codecForName("UTF-8")->toUnicode("<h3>About 2048-EZX</h3><br><font size=\"2\">"
+			"2048 Game implementation especially for Motorola EZX platform.<br><br>Version: 1.0, %1<br>"
+			"© EXL (exl@bk.ru)<br>"
+			"<u>https://github.com/EXL/2048</u></font>").arg(__DATE__));
+		const QString iconPath = QString("%1/icon_usr.png").arg(QFileInfo(qApp->argv()[0]).dirPath(true));
+		if (QFile::exists(iconPath)) {
+			QPixmap icon(48, 48); // TODO: Proper icon size for EZX platform?
 			icon.load(iconPath);
-			msgDlg->setTitleIcon(icon);
-		}
+			msgDlg->setIconPixmap(icon);
+		} else
+			msgDlg->setIconPixmap(RES_ICON_Reader().getIcon("Dialog_Exclamatory_Mark", true));
 		msgDlg->exec();
 		delete msgDlg;
-		*/
+	}
+	void setTitleScore(int score) { titleBar->setText(QString(" 2048-EZX | Score: %1 ").arg(score)); }
+	void roundedOn() {
+		widget->setRounded(true);
+		tileMenu->setItemChecked(M_ROUNDED, true);
+		tileMenu->setItemChecked(M_RECTANGLE, false);
+	}
+	void roundedOff() {
+		widget->setRounded(false);
+		tileMenu->setItemChecked(M_ROUNDED, false);
+		tileMenu->setItemChecked(M_RECTANGLE, true);
+	}
+	void soundOnOff() {
+		sound = !sound;
+		menu->setItemChecked(M_SOUND, sound);
+		static_cast<ZApplication *>(qApp)->enableTouchSound(sound);
 	}
 public:
-	MainWidget() : ZMainWidget(false, NULL, NULL, 0) {
-		QLabel *titleBar = new QLabel("2048-EZX", this);
-		titleBar->setScaledContents(true); // TODO: ??
+	MainWidget() : ZMainWidget(false, NULL, NULL, 0), sound(true) {
+/*	TODO: Improve this shit.
+		QWidget *titleBar = new QWidget(this, NULL, 0);
 		setTitleBarWidget(titleBar);
+		QHBoxLayout *layout = new QHBoxLayout(titleBar);
+//		titleBar->setLayout();
+		QLabel *title = new QLabel(" 2048-EZX", titleBar, NULL, WStyle_Customize);
+		//title->setBackgroundMode(titleBar->backgroundMode());
+		//title->setBackgroundOrigin(ParentOrigin);
+		QLabel *score = new QLabel("Score: 888888 ", titleBar, NULL, WStyle_Customize);
+		//score->setBackgroundMode(backgroundMode());
+		//score->setBackgroundOrigin(WidgetOrigin);
+//		score->setBackgroundMode(titleBar->backgroundMode());
+		score->setBackgroundMode(PaletteBase);
+		//score->setBackgroundPixmap(titleBar->backgroundPixmap());
+		layout->addWidget(title);
+		layout->addStretch();
+		layout->addWidget(score);
+		//titleBar->setLayout(layout);
+		// QLabel *titleBar = new QLabel(" 2048-EZX", this);
+		//titleBar->setScaledContents(true); // TODO: ??
+*/
+		titleBar = new QLabel(this, NULL);
+		titleBar->setText(" 2048-EZX | Score: 0 ");
+		setTitleBarWidget(titleBar);
+
+		widget = new Widget(this, NULL);
+		connect(widget, SIGNAL(scoreChanged(int)), this, SLOT(setTitleScore(int)));
+		setContentWidget(widget);
 
 		UTIL_CST *cst = new UTIL_CST(this, "Reset");
 		setCSTWidget(cst);
-		ZPushButton *leftButton = cst->getLeftBtn();
-		ZPushButton *middleButton = cst->getMidBtn();
-		ZPushButton *rightButton = cst->getRightBtn();
-		connect(rightButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+		connect(cst->getMidBtn(), SIGNAL(clicked()), widget, SLOT(reset()));
+		connect(cst->getRightBtn(), SIGNAL(clicked()), qApp, SLOT(quit()));
 
-		Widget *widget = new Widget(this, NULL);
-		setContentWidget(widget);
-
-
-		/*
-		setAppTitle(name);
-		Widget *widget = new Widget(this, "widget");
-		setContentWidget(widget);
-		ZSoftKey *softKeys = new ZSoftKey("CST_2A", this, this);
-		QRect menuRect = ZGlobal::getContentR();
-		ZOptionsMenu *menu = new ZOptionsMenu(menuRect, this, NULL, 0);
-		menu->insertItem("Take Screenshot", NULL, widget, SLOT(screenShotTimer()), true, false, false, 0, 0);
-		menu->insertSeparator(1, 1);
-		menu->insertItem("Save Game", NULL, widget, SLOT(save()), true, false, false, 2, 2);
-		menu->insertItem("Load Game", NULL, widget, SLOT(load()), true, false, false, 3, 3);
-		menu->insertItem("Reset Game", NULL, widget, SLOT(reset()), true, false, false, 4, 4);
-		menu->insertSeparator(5, 5);
-		menu->insertItem("About", NULL, this, SLOT(about()), true, false, false, 6, 6);
-		menu->insertItem(tr("TXT_RID_SOFTKEY_EXIT", "Exit"), NULL, qApp, SLOT(quit()), true, false, false, 7, 7);
-		softKeys->setOptMenu(ZSoftKey::LEFT, menu);
-		softKeys->setTextForOptMenuHide(tr("TXT_RID_SOFTKEY_OPTIONS", "Options"));
-		softKeys->setTextForOptMenuShow(tr("TXT_RID_SOFTKEY_SELECT", "Select"), tr("TXT_RID_SOFTKEY_CANCEL", "Cancel"));
-		softKeys->setText(ZSoftKey::LEFT, tr("TXT_RID_SOFTKEY_OPTIONS", "Options"));
-		softKeys->setText(ZSoftKey::RIGHT, tr("TXT_RID_SOFTKEY_EXIT", "Exit"));
-		softKeys->setClickedSlot(ZSoftKey::RIGHT, qApp, SLOT(quit()));
-		setSoftKey(softKeys);
-		*/
+		menu = new QPopupMenu(this);
+		menu->insertItem("Take Screenshot", widget, SLOT(screenShotTimer()), 0, M_SCREEN);
+		menu->insertSeparator();
+		menu->insertItem("Save Game", widget, SLOT(save()), 0, M_SAVE);
+		menu->insertItem("Load Game", widget, SLOT(load()), 0, M_LOAD);
+		menu->insertItem("Reset Game", widget, SLOT(reset()), 0, M_RESET);
+		menu->insertSeparator();
+		menu->insertItem("Sound", this, SLOT(soundOnOff()), 0, M_SOUND);
+		menu->setItemChecked(M_SOUND, true);
+		tileMenu = new QPopupMenu(this);
+		tileMenu->insertItem("Rounded", this, SLOT(roundedOn()), 0, M_ROUNDED);
+		tileMenu->insertItem("Rectangle", this, SLOT(roundedOff()), 0, M_RECTANGLE);
+		tileMenu->setItemChecked(M_ROUNDED, true);
+		menu->insertItem("Tiles", tileMenu, 0, M_TILES);
+		menu->insertSeparator();
+		menu->insertItem("About...", this, SLOT(about()), 0, M_ABOUT);
+		menu->insertItem("Exit", qApp, SLOT(quit()), 0, M_EXIT);
+		cst->getLeftBtn()->setPopup(menu);
 	}
 };
 
@@ -288,8 +296,6 @@ int main(int argc, char *argv[]) {
 	ZApplication application(argc, argv);
 	MainWidget mainWidget;
 	application.showMainWidget(&mainWidget);
-	// application.setMainWidget(&mainWidget);
-	// mainWidget.show();
 	return application.exec();
 }
 
