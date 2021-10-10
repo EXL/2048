0a5a8c9c5b9ae8540da570c3cd5dd3443a1afa69
EXL <exlmotodev@gmail.com>
2021-10-10 23:10:25 +0700

    [2048-MotoMAGX]: Fix creation of PEP-packages

--- util/build_pep_z6_and_zn5_packages.sh
+++ util/build_pep_z6_and_zn5_packages.sh
@@ -9,34 +9,32 @@ function clean {
 	cd -
 }
 
-function build_pep_Z6 {
-	cd 2048-MotoMAGX
-	make PLATFORM=EZX-Z6
-	mkdir -p 2048-MotoMAGX/app
-	cp 2048-MotoMAGX_Z6 2048-MotoMAGX/app/
+function create_pep {
 	cp pep/description.ini 2048-MotoMAGX/
-	sed -i "s/EXECUTABLE_FILE/2048-MotoMAGX_Z6/g" 2048-MotoMAGX/description.ini
+	sed -i "s/EXECUTABLE_FILE/2048/g" 2048-MotoMAGX/description.ini
 	cp ../image/icon/ic_launcher0.png 2048-MotoMAGX/app/icon_usr.png
 	cp ../image/icon/ic_launcher0.png 2048-MotoMAGX/icon_usr.png
 	cp ../image/icon/app-2048-Serenity-16.png 2048-MotoMAGX/icon_small_usr.png
-	tar -cvf 2048-MotoMAGX.tar 2048-MotoMAGX/
+	find 2048-MotoMAGX/ -printf "%P\n" | tar -cvf 2048-MotoMAGX.tar --no-recursion -C 2048-MotoMAGX/ -T -
 	gzip -9 2048-MotoMAGX.tar
+}
+
+function build_pep_Z6 {
+	cd 2048-MotoMAGX/
+	make PLATFORM=EZX-Z6
+	mkdir -p 2048-MotoMAGX/app
+	cp 2048-MotoMAGX_Z6 2048-MotoMAGX/app/2048
+	create_pep
 	mv 2048-MotoMAGX.tar.gz "../2048_MotoMAGX_Z6_v1.0_$date.pep"
 	cd -
 }
 
 function build_pep_ZN5 {
-	cd 2048-MotoMAGX
+	cd 2048-MotoMAGX/
 	make PLATFORM=EZX-ZN5
 	mkdir -p 2048-MotoMAGX/app
-	cp 2048-MotoMAGX_ZN5 2048-MotoMAGX/app/
-	cp pep/description.ini 2048-MotoMAGX/
-	sed -i "s/EXECUTABLE_FILE/2048-MotoMAGX_ZN5/g" 2048-MotoMAGX/description.ini
-	cp ../image/icon/ic_launcher0.png 2048-MotoMAGX/app/icon_usr.png
-	cp ../image/icon/ic_launcher0.png 2048-MotoMAGX/icon_usr.png
-	cp ../image/icon/app-2048-Serenity-16.png 2048-MotoMAGX/icon_small_usr.png
-	tar -cvf 2048-MotoMAGX.tar 2048-MotoMAGX/
-	gzip -9 2048-MotoMAGX.tar
+	cp 2048-MotoMAGX_ZN5 2048-MotoMAGX/app/2048
+	create_pep
 	mv 2048-MotoMAGX.tar.gz "../2048_MotoMAGX_ZN5_v1.0_$date.pep"
 	cd -
 }
