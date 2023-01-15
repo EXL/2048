1ea4c10190285607160f5a92ce5d15faaae0441d
EXL <exlmotodev@gmail.com>
2023-01-15 22:30:13 +0700

    [2048-UIS]: Update Makefiles and building script

--- 2048-P2K/make_ep1.bat
+++ 2048-P2K/make_ep1.bat
@@ -1,8 +1,8 @@
 :: This make.bat script was edited by EXL, 15-Jan-2023.
-:: Default platform is Motorola P2K, ElfPack v1.0, ADS1.2 [Build 848] on Windows.
+:: Default platform is Motorola P2K, ElfPack v1.x, ADS1.2 [Build 848] on Windows.
 :: Warning: `-nodebug` flag option for `armlink` is buggy.
 
-:: Uncomment it for verbose output.
+:: Uncomment it for disable verbose output.
 :: @echo off
 
 :: Compiler path.
@@ -28,18 +28,24 @@ set INCLUDES=-I..\src\ -I..\image\icon\precompiled\
 set OPTIM=-O2
 
 :: Output names.
-set O_NAME=2048
+set ENG_NAME=2048
 set ELF_NAME=2048-UIS
+set FINAL_NAME=2048-P2K-UIS_EP1
 
 :: Compiling step.
-%ARM_PATH%\tcc -I%SDK_PATH% %INCLUDES% %DEFINES% %OPTIM% -bigend -apcs /interwork -O2 -c ..\src\%O_NAME%.c -o %O_NAME%.o
-%ARM_PATH%\tcc -I%SDK_PATH% %INCLUDES% %DEFINES% %OPTIM% -bigend -apcs /interwork -O2 -c %ELF_NAME%.c -o %ELF_NAME%.o
+%ARM_PATH%\tcc -I%SDK_PATH% %INCLUDES% %DEFINES% %OPTIM% -bigend -apcs /interwork ^
+	-c ..\src\%ENG_NAME%.c -o %ENG_NAME%.o
+%ARM_PATH%\tcc -I%SDK_PATH% %INCLUDES% %DEFINES% %OPTIM% -bigend -apcs /interwork ^
+	-c %ELF_NAME%.c -o %ELF_NAME%.o
 
 :: Linking step.
-%ARM_PATH%\armlink -nolocals -reloc -first %LIB_MAIN%(Lib) %ELF_NAME%.o %O_NAME%.o %LIB_PATH%\%LIB_MAIN% -o %ELF_NAME%.elf
+%ARM_PATH%\armlink -nolocals -reloc -first %LIB_MAIN%(Lib) %ELF_NAME%.o %ENG_NAME%.o ^
+	%LIB_PATH%\%LIB_MAIN% -o %ELF_NAME%.elf
+
+:: Renaming step.
+move /y %ELF_NAME%.elf %FINAL_NAME%.elf
 
 if /I "%1"=="clean" (
-	del %O_NAME%.o
-	del %ELF_NAME%.o
-	del %ELF_NAME%.elf
+	del *.o
+	del *.elf
 )
